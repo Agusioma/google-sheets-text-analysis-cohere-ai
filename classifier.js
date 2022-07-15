174144ee8ee7dc29f2a911d422cb8125fbe1909e
Agusioma <terryterrence200@gmail.com>
2022-07-15 12:35:44 +0300

    summarize test2

--- classifier.js
+++ classifier.js
@@ -1,7 +1,4 @@
 function myFunction() {
-    /*var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "Bearer iwXeHXJjhh5VyRlbj7i7hE2KxvrOz5l9SqMZZ8yM");*/
     var sheet = SpreadsheetApp.getActiveSheet();
     var data = sheet.getDataRange().getValues();
     const cars = [];
@@ -104,9 +101,6 @@ function logProductInfo() {
     Logger.log(cars)
 }
 function summarize() {
-    var myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", "Bearer iwXeHXJjhh5VyRlbj7i7hE2KxvrOz5l9SqMZZ8yM");
 
     var raw = JSON.stringify({
         "prompt": "Passage: Is Wordle getting tougher to solve? Players seem to be convinced that the game has gotten harder in recent weeks ever since The New York Times bought it from developer Josh Wardle in late January. The Times has come forward and shared that this likely isn’t the case. That said, the NYT did mess with the back end code a bit, removing some offensive and sexual language, as well as some obscure words There is a viral thread claiming that a confirmation bias was at play. One Twitter user went so far as to claim the game has gone to “the dusty section of the dictionary” to find its latest words.\n\nTLDR: Wordle has not gotten more difficult to solve.\n--\nPassage: ArtificialIvan, a seven-year-old, London-based payment and expense management software company, has raised $190 million in Series C funding led by ARG Global, with participation from D9 Capital Group and Boulder Capital. Earlier backers also joined the round, including Hilton Group, Roxanne Capital, Paved Roads Ventures, Brook Partners, and Plato Capital.\n\nTLDR: ArtificialIvan has raised $190 million in Series C funding.\n--\nPassage: The National Weather Service announced Tuesday that a freeze warning is in effect for the Bay Area, with freezing temperatures expected in these areas overnight. Temperatures could fall into the mid-20s to low 30s in some areas. In anticipation of the hard freeze, the weather service warns people to take action now.\n\nTLDR:",
@@ -123,14 +117,15 @@ function summarize() {
     });
 
     var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
+        'method': 'post',
+        'muteHttpExceptions': true,
+        'contentType': 'application/json',
+        'headers': {
+            'Authorization': 'Bearer iwXeHXJjhh5VyRlbj7i7hE2KxvrOz5l9SqMZZ8yM'
+        },
+        'payload': raw,
         redirect: 'follow'
     };
-
-    fetch("https://api.cohere.ai/small/generate", requestOptions)
-        .then(response => response.text())
-        .then(result => console.log(result))
-        .catch(error => console.log('error', error));
+    var response = UrlFetchApp.fetch("https://api.cohere.ai/small/classify", requestOptions)
+    Logger.log(response);
 }
